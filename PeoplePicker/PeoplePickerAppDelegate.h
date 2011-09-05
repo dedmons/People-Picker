db88ab4d4e219a0f0febd39021045b4dd246767c
DJ Edmonson <dedmons@g.clemson.edu>
2011-09-05 13:29:29 -0400

    Initial Commit

---
+++ PeoplePicker/PeoplePickerAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  PeoplePickerAppDelegate.h
+//  PeoplePicker
+//
+//  Created by Douglas Edmonson on 9/5/11.
+//  Copyright 2011 Clemson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class PeoplePickerViewController;
+
+@interface PeoplePickerAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet PeoplePickerViewController *viewController;
+
+@end
---
+++ PeoplePicker/PeoplePickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PeoplePickerViewController.h
+//  PeoplePicker
+//
+//  Created by Douglas Edmonson on 9/5/11.
+//  Copyright 2011 Clemson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PeoplePickerViewController : UIViewController
+
+@end
